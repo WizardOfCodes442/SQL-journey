3c27170b671aadc3750da3869985b9c35b53160c
SpaceTech1800 <oliverspace1800@hotmail.com>
2024-12-13 00:50:17 -0800

    first commit

---
+++ addingdata001.sql
@@ -0,0 +1,6 @@
+/** you can also use sql to insert data ino the data base
+open new office*/
+INSERT INTO OFFICES (CITY, REGION, TARGETS, SALES, OFFICE) VALUE('Dallas', 'Western', 275000.00, 0.00, 23);
+
+/*signup new customer*/
+INSERT INTO CUSTOMERS (COMPANY, CUST_REP, CUST_NUM, CREDIT_LIMIT) VALUES('Acme ')
